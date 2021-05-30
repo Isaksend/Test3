d74aacfd90e88543c650a7eb97ba393af419c240
Isaksend <83396222+Isaksend@users.noreply.github.com>
2021-05-31 05:49:40 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var Preview = document.querySelector(".VideoPreview");
+var Popup = document.querySelector(".popupBlock");
+
+function OpenPopup(){
+    Popup.style.display = "block";
+} 
+function ClosePopup(){
+    Popup.style.display = "none";
+}
